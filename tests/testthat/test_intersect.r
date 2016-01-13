32c30584a8180547147b8035aa9a030c4368b90c
Jay Hesselberth <jay.hesselberth@gmail.com>
2016-01-12 19:58:35 -0700

    more test and files for tests

--- tests/testthat/test_intersect.r
+++ tests/testthat/test_intersect.r
@@ -1,25 +1,61 @@
-library(dplyr)
 library(Rbedtools)
+library(dplyr)
+context('intersect_cpp')
 
-# bed1_df <- data.frame(
-#   start = c(100, 150, 600),
-#   end   = c(200, 250, 800)
-# )
-# bed2_df <- data.frame(
-#   start = c(175, 175, 799),
-#   end   = c(200, 225, 900)
-# )
-bed1_df <- tibble(
-  ~start,    ~end,
-  100,       200,
-  150,       250,
-  600,       800
-)
-bed2_df <- tibble(
-  ~start,    ~end,
-  175,       200,
-  175,       225,
-  799,       900
-)
+test_that("simple overlap", {
+   bed1_df <- tibble(
+    ~start,    ~end,
+    100,       200,
+    150,       250
+  )
+  
+  bed2_df <- tibble(
+    ~start,    ~end,
+    175,       200,
+    175,       225
+  )
+  
+  res <- intersect_cpp(bed1_df, bed2_df)
+  expect_equal(nrow(res), 4)   
+})
 
-intersect_cpp(bed1_df, bed2_df)
+test_that("multple a's", {
+   bed1_df <- tibble(
+    ~start,    ~end,
+    100,       200,
+    100,       200,
+    100,       200,
+    100,       200,
+    100,       200
+  )
+  
+  bed2_df <- tibble(
+    ~start,    ~end,
+    175,       200
+  )
+  
+  res <- intersect_cpp(bed1_df, bed2_df)
+  expect_equal(nrow(res), 5)   
+})
+
+  
+test_that("multple b's", {
+   bed1_df <- tibble(
+    ~start,    ~end,
+    100,       200
+  )
+  
+  bed2_df <- tibble(
+    ~start,    ~end,
+    175,       200,
+    175,       200,
+    175,       200,
+    175,       200,
+    175,       200
+  )
+  
+  res <- intersect_cpp(bed1_df, bed2_df)
+  expect_equal(nrow(res), 5)   
+})
+
+  
--- tests/testthat/test_read_bed.r
+++ tests/testthat/test_read_bed.r
@@ -1,4 +1,23 @@
+library(Rbedtools)
 context("reading BED files")
 
-bed_tbl <- read_bed('bedfile')
+bed3_tbl <- read_bed('3fields.bed.gz')
 
+test_that('read BED3', {
+  expect_equal(ncol(bed3_tbl), 3)
+  expect_equal(nrow(bed3_tbl), 10)
+})
+
+bed6_tbl <- read_bed('6fields.bed.gz', n_fields = 6)
+
+test_that('read BED6', {
+  expect_equal(ncol(bed6_tbl), 6)
+  expect_equal(nrow(bed6_tbl), 10)
+})
+
+bedgraph_tbl <- read_bedgraph('test.bg.gz')
+
+test_that('read bedGraph', {
+  expect_equal(ncol(bedgraph_tbl), 4)
+  expect_equal(nrow(bedgraph_tbl), 4)
+})
