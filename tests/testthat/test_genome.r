d8b1ee404cf905085115637cfab58c9cb51cefbb
Jay Hesselberth <jay.hesselberth@gmail.com>
2016-01-13 09:32:05 -0700

    use test files in inst/extdata

---
+++ tests/testthat/test_genome.r
@@ -0,0 +1,14 @@
+library(Rbedtools)
+context("genome files")
+
+library(dplyr)
+
+genome_path <- system.file('extdata', 'hg19.chrom.sizes.gz', package = 'Rbedtools')
+genome <- read_genome(genome_path)
+
+test_that("genomes are correctly read", {
+  expect_equal(nrow(genome), 93)
+  expect_equal(colnames(genome), c('chrom','size'))
+  expect_is(genome, "data.frame")
+})
+
--- tests/testthat/test_read_bed.r
+++ tests/testthat/test_read_bed.r
@@ -1,21 +1,25 @@
 library(Rbedtools)
 context("reading BED files")
 
-bed3_tbl <- read_bed('3fields.bed.gz')
+bed3_path <- system.file('extdata', '3fields.bed.gz', package = 'Rbedtools')
+bed6_path <- system.file('extdata', '6fields.bed.gz', package = 'Rbedtools')
+bedgraph_path <- system.file('extdata', 'test.bg.gz', package = 'Rbedtools')
+
+bed3_tbl <- read_bed(bed3_path)
 
 test_that('read BED3', {
   expect_equal(ncol(bed3_tbl), 3)
   expect_equal(nrow(bed3_tbl), 10)
 })
 
-bed6_tbl <- read_bed('6fields.bed.gz', n_fields = 6)
+bed6_tbl <- read_bed(bed6_path, n_fields = 6)
 
 test_that('read BED6', {
   expect_equal(ncol(bed6_tbl), 6)
   expect_equal(nrow(bed6_tbl), 10)
 })
 
-bedgraph_tbl <- read_bedgraph('test.bg.gz')
+bedgraph_tbl <- read_bedgraph(bedgraph_path)
 
 test_that('read bedGraph', {
   expect_equal(ncol(bedgraph_tbl), 4)
