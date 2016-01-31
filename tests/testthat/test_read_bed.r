81cf75124ba88db8f4a7603111a77ba350829705
Jay Hesselberth <jay.hesselberth@gmail.com>
2016-01-31 08:22:31 -0700

    test read_bed12

--- tests/testthat/test_read_bed.r
+++ tests/testthat/test_read_bed.r
@@ -1,8 +1,8 @@
-library(Rbedtools)
 context("reading BED files")
 
 bed3_path <- system.file('extdata', '3fields.bed.gz', package = 'Rbedtools')
 bed6_path <- system.file('extdata', '6fields.bed.gz', package = 'Rbedtools')
+bed12_path <- system.file('extdata', 'mm9.bed12.gz', package = 'Rbedtools')
 bedgraph_path <- system.file('extdata', 'test.bg.gz', package = 'Rbedtools')
 
 bed3_tbl <- read_bed(bed3_path)
@@ -19,17 +19,14 @@ test_that('read BED6', {
   expect_equal(nrow(bed6_tbl), 10)
 })
 
+bed12_tbl <- read_bed12(bed12_path)
+test_that('read BED12', {
+  expect_equal(ncol(bed12_tbl), 12)
+})
+
 bedgraph_tbl <- read_bedgraph(bedgraph_path)
 
 test_that('read bedGraph', {
   expect_equal(ncol(bedgraph_tbl), 4)
   expect_equal(nrow(bedgraph_tbl), 4)
 })
-
-# sorted_tbl <- read_bed('3fields.bed.gz')
-# unsorted_tbl <- read_bed('3fields.bed.gz', sort = FALSE)
-# 
-# test_that('sort attribute is set correctly', {
-#   expect_equal(attr(sorted_tbl, 'sorted'), TRUE) 
-#   expect_equal(attr(unsorted_tbl, 'sorted'), FALSE) 
-# })
