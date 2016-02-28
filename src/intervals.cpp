0a707162c782df449d132cb1fb6a425119a29c06
jayhesselberth <jay.hesselberth@gmail.com>
2016-02-28 14:58:06 -0700

    reorg intervals.h/cpp

---
+++ src/intervals.cpp
@@ -0,0 +1,44 @@
+
+#include <Rcpp.h>
+#include "intervals.h"
+
+// calculate overlap between two intervals
+int intervalOverlap(const interval_t a, const interval_t b) {
+  return std::min(a.end, b.end) - std::max(a.start, b.start);  
+}
+
+// is interval a after interval b?
+bool intervalAfter(const interval_t a, const interval_t b) {
+  if (a.start > b.end) {
+    return true ;
+  } else {
+    return false ;
+  } 
+}
+
+std::list <interval_t> createIntervals(Rcpp::DataFrame df) {
+ 
+  std::vector <std::string> df_chrom_v = 
+    Rcpp::as< std::vector <std::string> >(df["chrom"]); 
+  
+  std::vector <int> df_start_v =
+    Rcpp::as< std::vector <int> >(df["start"]); 
+  
+  std::vector <int> df_end_v =
+    Rcpp::as< std::vector <int> >(df["end"]); 
+  
+  std::list <interval_t> intervals ;
+  
+  for (unsigned i = 0; i < df_start_v.size(); ++i) {
+    
+    interval_t interval;
+    
+    interval.chrom = df_chrom_v[i] ;   
+    interval.start = df_start_v[i] ;
+    interval.end = df_end_v[i] ;
+    
+    intervals.push_back(interval) ;
+  }
+  
+  return intervals ;  
+} 
--- src/intervals.h
+++ src/intervals.h
@@ -1,50 +1,21 @@
 #ifndef INTERVALS_INCLUDE
+#define INTERVALS_INCLUDE
 
 struct interval_t {
-  char chrom ;
+  std::string chrom ;
   int start, end ;
-} Interval ;
+};
 
 struct intersection_t {
   interval_t a, b ;
-} Intersection ;
+};
 
 // calculate overlap between two intervals
-int intervalOverlap(const interval_t a, const interval_t b) {
-  return std::min(a.end, b.end) - std::max(a.start, b.start);  
-}
+int intervalOverlap(const interval_t a, const interval_t b) ;
 
 // is interval a after interval b?
-bool intervalAfter(const interval_t a, const interval_t b) {
-  if (a.start > b.end) {
-    return true ;
-  } else {
-    return false ;
-  } 
-}
+bool intervalAfter(const interval_t a, const interval_t b) ;
 
-std::list <interval_t> createIntervals(Rcpp::DataFrame df) {
-  
-  std::vector <double> df_start_v =
-    Rcpp::as< std::vector <double> >(df["start"]); 
-  
-  std::vector <double> df_end_v =
-    Rcpp::as< std::vector <double> >(df["end"]); 
-  
-  std::list <interval_t> intervals ;
-  
-  for (unsigned i = 0; i < df_start_v.size(); ++i) {
-    
-    interval_t interval;
-    
-    interval.start = df_start_v[i] ;
-    interval.end = df_end_v[i] ;
-    
-    intervals.push_back(interval) ;
-  }
-  
-  return intervals ;  
-} 
+std::list <interval_t> createIntervals(Rcpp::DataFrame df) ;
 
-#define INTERVALS_INCLUDE
 #endif /* INTERVALS_INCLUDE */
