9263dd94146bda2f6e5f61802363aa6cc1d8c7ca
jayhesselberth <jay.hesselberth@gmail.com>
2016-02-28 16:50:00 -0700

    add sorted tests

--- tests/testthat/test_read_bed.r
+++ tests/testthat/test_read_bed.r
@@ -10,6 +10,7 @@ bed3_tbl <- read_bed(bed3_path)
 test_that('read BED3', {
   expect_equal(ncol(bed3_tbl), 3)
   expect_equal(nrow(bed3_tbl), 10)
+  expect_true(attr(bed3_tbl, "sorted"))
 })
 
 bed6_tbl <- read_bed(bed6_path, n_fields = 6)
@@ -17,11 +18,13 @@ bed6_tbl <- read_bed(bed6_path, n_fields = 6)
 test_that('read BED6', {
   expect_equal(ncol(bed6_tbl), 6)
   expect_equal(nrow(bed6_tbl), 10)
+  expect_true(attr(bed6_tbl, "sorted"))
 })
 
 bed12_tbl <- read_bed12(bed12_path)
 test_that('read BED12', {
   expect_equal(ncol(bed12_tbl), 12)
+  expect_true(attr(bed12_tbl, "sorted"))
 })
 
 bedgraph_tbl <- read_bedgraph(bedgraph_path)
@@ -29,4 +32,5 @@ bedgraph_tbl <- read_bedgraph(bedgraph_path)
 test_that('read bedGraph', {
   expect_equal(ncol(bedgraph_tbl), 4)
   expect_equal(nrow(bedgraph_tbl), 4)
+  expect_true(attr(bedgraph_tbl, "sorted"))
 })
