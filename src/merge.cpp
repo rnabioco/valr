6b3c714f331242493cdf25aed6e9cfd6bd56d4c1
jayhesselberth <jay.hesselberth@gmail.com>
2016-03-03 06:15:19 -0700

    fix interval_overlap() bug in bed_merge() (returns int)

--- src/merge.cpp
+++ src/merge.cpp
@@ -24,9 +24,8 @@ store_intervals(std::stack<interval_t>& chrom_intervals, std::list<interval_t>& 
 std::list <interval_t>
 merge_intervals(std::list <interval_t> intervals) {
 
+  // make an empty prev_interval to start with
   interval_t prev_interval = make_interval("", 0, 0) ;
-  int interval_count = 0 ;
-  int if_case = 0 ;
   
   std::list <interval_t> merged_intervals ;
   std::stack <interval_t> chrom_intervals ;
@@ -35,57 +34,26 @@ merge_intervals(std::list <interval_t> intervals) {
   for (it = intervals.begin(); it != intervals.end(); ++it) {
  
     interval_t curr_interval = *it ;
-   
-    Rcerr << "eval interval: " << curr_interval.chrom << ":" << curr_interval.start << "-" << curr_interval.end << std::endl ; 
-    // first interval on chrom
-    if ( interval_count == 0 ) {
+  
+    // switch chroms, can "switch" onto first chrom from null first prev_interval
+    if ( curr_interval.chrom != prev_interval.chrom ) {
       
-      if ( it == intervals.begin() ) {
-        Rcerr << "very first interval!" << std::endl ;
-        // first interval on first chrom
-        chrom_intervals.push(curr_interval) ;
-        // ++interval_count ;
-        // prev_interval = *it ;
-        // continue ;
-        // 
-      } else {
-        // prev_interval is first interval on curr chrom
-        Rcerr << "first interval on new chrom" << std::endl ;
-        chrom_intervals.push(prev_interval) ;
-      }
-      
-      if_case = 1 ;   
-    }
-   
-    // switched chromosomes
-    else if ( curr_interval.chrom != prev_interval.chrom ) {
-      
-      Rcerr << "switched chroms" << std::endl ;
       // store current set of intervals
       store_intervals(chrom_intervals, merged_intervals) ;
+      
+      // store curr, which is the first on the new chrom
       chrom_intervals.push(curr_interval) ;
-      interval_count = 0 ;
-      // prev_interval = *it ;
-      // continue ;
-      // 
-      if_case = 2 ;   
     } 
 
     else if (interval_overlap(curr_interval, chrom_intervals.top()) > 0) {
       // update the stack interval with new end
       chrom_intervals.top().end = curr_interval.end ;
-      
-      if_case = 3 ;   
     }
     
     else {
       chrom_intervals.push(curr_interval) ;
-      if_case = 4 ;   
     }
     
-    Rcerr << "if case: " << if_case << std::endl ;
-    
-    ++interval_count ;
     prev_interval = *it ;
     
   }
