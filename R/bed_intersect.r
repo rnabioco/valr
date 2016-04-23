dd2062fa90962ac12ee4f23a09318185385bfac0
Jay Hesselberth <jay.hesselberth@gmail.com>
2016-04-23 07:05:26 -0600

    pure r impl of bed_intersect

--- R/bed_intersect.r
+++ R/bed_intersect.r
@@ -1,21 +1,65 @@
 #' instersect BED intervals
 #' 
-#' @param bed_tbl_a BED intervals 
-#' @param bed_tbl_b BED intervals 
+#' @param x BED intervals 
+#' @param y BED intervals 
+#' @param max_dist maximum distance between intserections
+#' @param strand intersect intervals on same strand
 #' 
+#' @examples 
+#' x <- tibble::frame_data(
+#' ~chrom, ~start, ~end,
+#' "chr1", 100,    500,
+#' "chr2", 200,    400,
+#' "chr2", 300,    500,
+#' "chr2", 800,    900
+#' )
+#' 
+#' y <- tibble::frame_data(
+#' ~chrom, ~start, ~end,
+#' "chr1", 150,    400,
+#' "chr2", 230,    430,
+#' "chr2", 350,    430
+#' )
+#'
+#' bed_intersect(x, y)
+#' bed_intersect(x, y, max_dist = 50)
+#'  
 #' @seealso \url{http://bedtools.readthedocs.org/en/latest/content/tools/intersect.html}
 #'  
 #' @export
-bed_intersect <- function(bed_tbl_a, bed_tbl_b) {
-
-  if ( ! is_sorted(df_a) ) {
-    df_a <- bed_sort(df_a)
-  }
-  if ( ! is_sorted(df_b) ) {
-    df_b <- bed_sort(df_b)
-  }
+bed_intersect <- function(x, y, max_dist = 0, strand = FALSE, strand_opp = FALSE) {
  
-  res <- intersect_impl(df_a, df_b)
+  max_rows <- 100
+  if (nrow(x) > max_rows || nrow(y) > max_rows) {
+    stop("max rows exceeded for r impl of bed_intersect")
+  } 
+  
+  if ( ! is_sorted(x) )
+    x <- bed_sort(x)
+  if ( ! is_sorted(y) )
+    y <- bed_sort(y)
+  
+  if (is.null(groups(x)) || groups(x) != "chrom")
+    x <- group_by(x, chrom)
+  if (is.null(groups(y)) || groups(y) != "chrom")
+    y <- group_by(y, chrom)
+ 
+  res <- intersect_rimpl(x, y, max_dist)
+  
+  if (strand) {
+    res <- filter(res, strand.x == strand.y) 
+  } else if (strand_opp) {
+    res <- filter(res, strand.x != strand.y) 
+  }
   
   res
+}
+
+#' pure R impl of intersect
+intersect_rimpl <- function(x, y, max_dist) {
+  full_join(x, y, by = 'chrom') %>%
+    rowwise() %>%
+    mutate(.overlap = min(end.x, end.y) - max(start.x, start.y)) %>%
+    filter(.overlap > max_dist) %>%
+    select(-.overlap) %>% ungroup()
 }
