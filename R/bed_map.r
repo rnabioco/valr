9eb0a7585add92d0d0f0f2f7b96111e22fa7518a
Jay Hesselberth <jay.hesselberth@gmail.com>
2016-04-24 07:22:56 -0600

    rewrite bed_map helpers and examples

--- R/bed_map.r
+++ R/bed_map.r
@@ -18,9 +18,23 @@
 #'  "chr1", 100, 250, 10,
 #'  "chr1", 150, 250, 20,
 #'  "chr2", 250, 500, 500)
-#'  
-#' bed_map(bed_tbl, signal_tbl, sum = sum(value), max = max(value))
-#' bed_map(bed_tbl, signal_tbl, concat = concat(value))
+#' 
+#' # mean, median, sd etc
+#' bed_map(bed_tbl, signal_tbl, sum = sum(value))
+#' bed_map(bed_tbl, signal_tbl, min = min(value), max = max(value))
+#' 
+#' bed_map(bed_tbl, signal_tbl, concat(value))
+#' bed_map(bed_tbl, signal_tbl, distinct(value))
+#' bed_map(bed_tbl, signal_tbl, first(value))
+#' bed_map(bed_tbl, signal_tbl, last(value))
+#' 
+#' bed_map(bed_tbl, signal_tbl, absmax = abs(max(value)))
+#' bed_map(bed_tbl, signal_tbl, absmin = abs(min(value)))
+#' bed_map(bed_tbl, signal_tbl, count = length(value))
+#' bed_map(bed_tbl, signal_tbl, count_distinct = length(unique(value)))
+#' 
+#' # use decreasing = TRUE to reverse numbers
+#' bed_map(bed_tbl, signal_tbl, distinct_num = distinct(sort(value)))
 #' 
 #' @export
 bed_map <- function(bed_tbl, signal_tbl, ...) {
@@ -36,38 +50,20 @@ bed_map <- function(bed_tbl, signal_tbl, ...) {
 
 #' @export
 #' @rdname bed_map
-absmax <- function(.data) {
-  abs(max(.data))
-}
-
-#' @export
-#' @rdname bed_map
-absmin <- function(.data) {
-  abs(min(.data))
-}
-
-#' @export
-#' @rdname bed_map
 concat <- function(.data, sep = ',') {
   paste0(.data, collapse = sep)
 }
 
 #' @export
 #' @rdname bed_map
-distinct <- function(.data, sep = ',') {
+distinct_only <- function(.data, sep = ',') {
   concat(unique(.data), sep = sep)
 }
 
 #' @export
 #' @rdname bed_map
-count <- function(.data) {
-  length(.data)
-}
-
-#' @export
-#' @rdname bed_map
-count_distinct <- function(.data) {
-  length(unique(.data))
+distinct <- function(.data, sep = ',') {
+  concat(rle(.data)$values, sep = sep)
 }
 
 #' @export
