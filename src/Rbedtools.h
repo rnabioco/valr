3544faf42f84ba2d63846bd541686119c0aeb2f8
Jay Hesselberth <jay.hesselberth@gmail.com>
2016-04-25 21:21:19 -0600

    rm unused cxx code

--- src/Rbedtools.h
+++
@@ -1,7 +0,0 @@
-#ifndef Rbedtools_Rbedtools_H
-#define Rbedtools_Rbedtools_H
-
-#include "Rbedtools/intervals.h"
-#include "Rbedtools/genome.h"
-
-#endif
--- src/Rbedtools/genome.h
+++
@@ -1,6 +0,0 @@
-#ifndef Rbedtools_genome_H 
-#define Rbedtools_genome_H 
-
-std::map<std::string, int> create_genome(Rcpp::DataFrame genome) ;
-  
-#endif 
