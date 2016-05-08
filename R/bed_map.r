e809ea2bb9d66265f4f3c1cf5fc138fdeefdbd15
Jay Hesselberth <jay.hesselberth@gmail.com>
2016-05-08 07:16:23 -0600

    new `add_group` param, revise grouping strategy

--- R/bed_map.r
+++ R/bed_map.r
@@ -2,6 +2,7 @@
 #' 
 #' @param x tbl of intervals
 #' @param y tbl of signals
+#' @param add_group additional grouping vars
 #' @param ... name-value pairs of summary functions like \code{\link{min}()}, 
 #'   \code{\link{count}()}, \code{\link{concat}()}. 
 #'   
@@ -39,10 +40,11 @@
 #' bed_map(x, y, distinct_num = distinct(sort(value.y)))
 #' 
 #' @export
-bed_map <- function(x, y, ...) {
+bed_map <- function(x, y, ..., add_group = NULL) {
 
+  groups_default <- c('chrom', 'start.x', 'end.x')
   res <- bed_intersect(x, y) %>%
-    group_by(chrom, start.x, end.x) %>%
+    group_by_(.dots = c(groups_default, add_group)) %>%
     summarize_(.dots = lazyeval::lazy_dots(...))
 
   res 
