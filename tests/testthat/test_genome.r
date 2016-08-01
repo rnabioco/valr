6d4ac181e9809c8ba147db138d2770aac210b94c
Jay Hesselberth <jay.hesselberth@gmail.com>
2016-08-01 17:01:19 -0600

    up test coverage for genome, map

--- tests/testthat/test_genome.r
+++ tests/testthat/test_genome.r
@@ -11,11 +11,20 @@ test_that("genomes are correctly read", {
 
 test_that("unbounded intervals are removed", {
   
-  bed_tbl <- tibble::frame_data(
+  x <- tibble::frame_data(
    ~chrom, ~start, ~end,
    "chr1", -100,   500,
    "chr1", 100,    1e9,
    "chr1", 500,    1000
   )
-  expect_equal(nrow(bound_intervals(bed_tbl, genome)), 1)
+  expect_equal(nrow(bound_intervals(x, genome)), 1)
+})
+
+test_that("trim param removes dangling intervals", {
+  x <- tibble::frame_data(
+   ~chrom, ~start, ~end,
+   "chr1", 1,      249250721
+  )
+  res <- bound_intervals(x, genome, trim = TRUE)
+  expect_equal(res$end, 249250621)
 })
--- tests/testthat/test_map.r
+++ tests/testthat/test_map.r
@@ -35,3 +35,94 @@ test_that("x/y groupings are respected", {
   res <- bed_map(x, y, vals = sum(value.y))
   expect_equal(res$vals, c(10,20,500,500))
 })
+
+test_that("concat works correctly", {
+  x <- tibble::frame_data(
+    ~chrom, ~start, ~end,
+    "chr1", 100, 250,
+    "chr2", 250, 500,
+    "chr2", 250, 500
+  )
+  
+  y <- tibble::frame_data(
+    ~chrom, ~start, ~end, ~value,
+    "chr1", 100, 250, 10,
+    "chr1", 150, 250, 20,
+    "chr2", 250, 500, 500
+  )
+  
+  res <- bed_map(x, y, vals = concat(value.y))
+  expect_equal(res$vals, c("10,20", "500,500"))
+})
+
+test_that("values works correctly", {
+   x <- tibble::frame_data(
+    ~chrom, ~start, ~end,
+    "chr1", 100, 250
+   )
+  
+   y <- tibble::frame_data(
+    ~chrom, ~start, ~end, ~value,
+    "chr1", 100, 250, 10,
+    "chr1", 100, 250, 20,
+    "chr1", 150, 250, 10,
+    "chr1", 150, 250, 20
+   )
+
+  res <- bed_map(x, y, vals = values(value.y))
+  expect_equal(res$vals, c("10,20,10,20"))
+})
+
+test_that("values_unique works correctly", {
+  x <- tibble::frame_data(
+    ~chrom, ~start, ~end,
+    "chr1", 100, 250
+  )
+  
+  y <- tibble::frame_data(
+    ~chrom, ~start, ~end, ~value,
+    "chr1", 100, 250, 10,
+    "chr1", 150, 250, 20,
+    "chr1", 100, 250, 10,
+    "chr1", 150, 250, 20
+  )
+  
+  res <- bed_map(x, y, vals = values_unique(value.y))
+  expect_equal(res$vals, c("10,20"))
+})
+
+test_that("first works correctly", {
+  x <- tibble::frame_data(
+    ~chrom, ~start, ~end,
+    "chr1", 100, 250
+  )
+  
+  y <- tibble::frame_data(
+    ~chrom, ~start, ~end, ~value,
+    "chr1", 100, 250, 10,
+    "chr1", 150, 250, 20,
+    "chr1", 100, 250, 30,
+    "chr1", 150, 250, 40
+  )
+  
+  res <- bed_map(x, y, vals = first(value.y))
+  expect_equal(res$vals, 10)
+})
+
+test_that("last works correctly", {
+  x <- tibble::frame_data(
+    ~chrom, ~start, ~end,
+    "chr1", 100, 250
+  )
+  
+  y <- tibble::frame_data(
+    ~chrom, ~start, ~end, ~value,
+    "chr1", 100, 250, 10,
+    "chr1", 150, 250, 20,
+    "chr1", 100, 250, 30,
+    "chr1", 150, 250, 40
+  )
+  
+  res <- bed_map(x, y, vals = last(value.y))
+  expect_equal(res$vals, 40)
+})
