440dcdbc7366f1dad36e9c81b12a9f41955cad06
Jay Hesselberth <jay.hesselberth@gmail.com>
2016-08-01 08:39:46 -0600

    cleanup package file

--- R/valr.r
+++ R/valr.r
@@ -1,8 +1,8 @@
 #' valr: genome interval arithmetic in R
 #' 
 #' valr provides tools to read and manipulate intervals and signals on a 
-#' genome reference. While other analysis suites like BEDtools and BEDOPS enable
-#' fast command line analysis, valr enables similar analyses \emph{within
+#' genome reference. While other analysis suites enable
+#' command-line analysis, valr enables similar analyses \emph{within
 #' R}.
 #' 
 #' valr focuses on manipulating intervals in BED format and signal in 
@@ -21,11 +21,12 @@
 #' @seealso \url{https://pythonhosted.org/pybedtools/}
 #' @seealso \url{http://bedops.readthedocs.org/en/latest/index.html}
 #' @seealso \url{https://bioconductor.org/packages/release/bioc/html/GenomicRanges.html}
+#' @seealso \url{https://cran.r-project.org/web/packages/bedr/index.html}
 #'   
 #' @useDynLib valr
 #' @importFrom tibble frame_data
 #' @importFrom readr read_tsv col_integer col_character
-#' @importFrom stringr str_replace str_split str_c str_length str_detect
+#' @importFrom stringr str_replace str_split str_c str_length
 #' @importFrom tidyr unnest gather separate spread
 #' @importFrom purrr by_row
 #' @importFrom lazyeval lazy_dots
