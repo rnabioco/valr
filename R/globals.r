07e20c08eff843f119b05bff36481594e1646af4
Jay Hesselberth <jay.hesselberth@gmail.com>
2016-09-08 19:03:06 -0600

    simplify global vars

--- R/globals.r
+++ R/globals.r
@@ -1,7 +1,7 @@
 globalVariables(c("chrom", "start", "end", "strand",
-                  "exon_sizes", "exon_starts", "exon_size", "exon_start", ".exon_num",
-                  ".exon_size", ".exon_start", "name", ".prev_end", "size",
+                  ".exon_num", ".exon_size", ".exon_start", "name", ".prev_end", "size",
                   "max.end", "size_mean", "int_est", ".starts", ".ends", ".row",
-                  ".start", ".end", ".size", ".interval_size",
-                  "start.x", "end.x", "start.y", "strand.x", "strand.y",
-                  ".overlap", ".merge_id", "value", ".win_num"))
+                  ".start", ".end", ".size",
+                  "start.x", "end.x", "strand.x",
+                  "start.y", "strand.x", "strand.y",
+                  ".overlap", ".id", ".value", ".win_num"))
