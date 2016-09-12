bdc5e6bafb2c56ce77c17ff17c53a6a570eb7db3
Jay Hesselberth <jay.hesselberth@gmail.com>
2016-09-12 14:41:52 -0600

    some progress

--- R/bed_shuffle.r
+++ R/bed_shuffle.r
@@ -5,27 +5,46 @@
 #' @param incl tbl of included intervals
 #' @param excl tbl of excluded intervals
 #' @param max_tries maximum tries to identify a bounded region
+#' @param within shuffle within chromosomes
+#' @param seed seed for reproducible intervals
 #' 
 #' @return \code{data_frame}
 #' 
 #' @seealso \url{http://bedtools.readthedocs.io/en/latest/content/tools/shuffle.html}
 #' 
 #' @examples 
-#' x <- tibble::frame_data(
+#' x <- tibble::tribble(
 #' )
 #' 
-#' genome <- tibble::frame_data(
+#' genome <- tibble::tribble(
 #' )
 #' 
 #' @export
-bed_shuffle <- function(x, genome, incl = NULL, excl = NULL, max_tries = 100) {
- 
-  # create empty data frames if incl / excl are null 
-  if (is.null(incl))
-    incl <- data_frame()
-  if (is.null(excl)) 
-    excl <- data_frame()
+bed_shuffle <- function(x, genome, incl = NULL, excl = NULL,
+                        within = FALSE, max_tries = 100, seed = 0) {
+
+  # flatten incl and excl
+  if (!is.null(incl))
+      incl <- bed_merge(incl)
+  if (!is.null(excl))
+      excl <- bed_merge(excl)
+      
+  # find the intervals to be sampled from 
+  if (is.null(incl) && is.null(excl)) {
+    interval_bounds <- genome
+  } else if (is.null(incl) && !is.null(incl)) {
+    interval_bounds <- bed_subtract(genome, excl)
+  } else if (!is.null(incl) && is.null(excl)) {
+    interval_bounds <- incl
+  } else {
+    interval_bounds <- bed_subtract(incl, excl)  
+  }
   
-  res <- shuffle_impl(x, genome, incl, excl, max_tries)
-  res
+  # remove chroms from genome that are not in interval bounds. this prevents sampling 
+  # excluded chroms on the Rcpp side.
+  genome <- dplyr::anti_join(genome, interval_bounds, by = 'chrom')
+  
+  res <- shuffle_impl(x, genome, interval_bounds, within, max_tries, seed)
+ 
+  res 
 }  
--- src/shuffle.cpp
+++ src/shuffle.cpp
@@ -1,22 +1,77 @@
 #include "valr.h"
 
 //[[Rcpp::export]]
-DataFrame shuffle_impl(DataFrame df, DataFrame incl, DataFrame excl, DataFrame genome,
-                       int max_tries) {
+DataFrame shuffle_impl(DataFrame df, DataFrame genome, DataFrame interval_bounds,
+                       bool within = false, int max_tries = 1000, int seed = 0) {
 
-  genome_map_t chrom_sizes = makeChromSizes(genome) ;
+  // seed for reproducible intervals
+  if (seed == 0)
+    seed = round(R::runif(0, RAND_MAX)) ;
+  
+  // seed the generator
+  auto generator = ENG(seed) ;
+  
+  // generated weighted chromosomes to sample from
+  CharacterVector chroms_genome = genome["chrom"] ;
+  NumericVector sizes_genome = genome["size"] ;
+  int nchrom = chroms_genome.size() ;
+  
+  // calculate weights for chrom distribution
+  float mass = sum(sizes_genome) ;
+  NumericVector weights = sizes_genome / mass ;
+  
+  Range chromidx(0, nchrom) ;
+  PDIST chrom_dist(chromidx.begin(), chromidx.end(), weights.begin()) ;
+
+  // now deal with query data frame
+  CharacterVector chroms_df = df["chrom"] ;
+  IntegerVector starts_df = df["start"] ;
+  IntegerVector ends_df = df["end"] ;
+  
+  IntegerVector sizes_df = ends_df - starts_df ;
+  
+  // number of rows input == rows in output
+  int nr = df.size() ;  
+  
+  CharacterVector chroms_out(nr) ;
+  IntegerVector starts_out(nr) ; 
+  IntegerVector ends_out(nr) ; 
  
-  IntegerVector starts = df["start"] ;
-  IntegerVector ends = df["end"] ;
-  
-  IntegerVector sizes = ends - starts ; 
-  
-  CharacterVector chroms = df["chrom"] ;
-  
-  std::vector<std::string> chroms_out ;
-  std::vector<int> starts_out ; 
-  std::vector<int> ends_out ; 
-
+  std::unordered_map<int, intervalTree> interval_trees ;
+ 
+  for (int i = 0; i<nr; ++i) {
+    
+    // select a chromosome 
+    if (within) {
+      chroms_out[i] = chroms_df[i] ;
+    } else {
+      auto chrom_idx = chrom_dist(generator) ;
+      chroms_out[i] = chroms_genome[chrom_idx] ;
+    }
+    
+    int rand_start = 0 ; 
+    int niter = 0 ;
+    bool inbounds = false ;
+    intervalVector overlaps ;
+    
+    while (!inbounds) {
+     
+      niter++ ;  
+   
+      tree->findOverlapping(rand_start, rand_end, overlaps) ;
+      
+      if (!overlaps.empty()) continue ;
+        
+      if (niter == max_tries) {
+        stop('maximum iterations exceeded in bed_shuffle') ;
+      }
+      
+    }  
+    
+    starts_out[i] = rand_start ;
+    ends_out[i] = rand_start + sizes_df[i] ;
+  } 
+ 
   return DataFrame::create( Named("chrom") = chroms_out,
                             Named("start") = starts_out,
                             Named("end") = ends_out) ;
@@ -25,14 +80,14 @@ DataFrame shuffle_impl(DataFrame df, DataFrame incl, DataFrame excl, DataFrame g
 /***R
 library(dplyr)
 library(valr)
-genome <- tibble::frame_data(
+genome <- tibble::tribble(
    ~chrom,  ~size,
    "chr1", 500,
    "chr2", 600,
    "chr3", 800
 ) 
 
-x <- tibble::frame_data(
+x <- tibble::tribble(
    ~chrom, ~start, ~end,
    "chr1", 100,    300,
    "chr1", 200,    400,
