7253a171e94a1306ddb574c6624629295cd9342a
Jay Hesselberth <jay.hesselberth@gmail.com>
2016-09-16 16:13:34 -0600

    tests for `bed12_to_exons`

---
+++ tests/testthat/test_bed12_to_exons.r
@@ -0,0 +1,12 @@
+context('bed12_to_exons')
+
+test_that('invalid ncol causes an error', {
+  x <- tibble::tribble()
+  expect_error(bed12_to_exons(x))
+})
+
+test_that('BED12 is parsed correctly', {
+  x <- read_bed12(system.file('extdata', 'mm9.bed12.gz', package = 'valr'))
+  expect_equal(nrow(bed12_to_exons(x)), 29)
+  expect_equal(ncol(bed12_to_exons(x)), 6)
+})
