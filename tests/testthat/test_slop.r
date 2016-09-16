36ab1c69507067a541aebc5ae44455422ac32151
Jay Hesselberth <jay.hesselberth@gmail.com>
2016-09-16 16:50:00 -0600

    test fraction and strand params in bed_slop

--- tests/testthat/test_slop.r
+++ tests/testthat/test_slop.r
@@ -5,7 +5,7 @@ genome <- tibble::tribble(
  "chr1", 5000
 )
 
-bed_df <- tibble::tribble(
+x <- tibble::tribble(
  ~chrom, ~start, ~end, ~name, ~score, ~strand,
  "chr1", 500,    1000, '.',   '.',     '+',
  "chr1", 1000,   1500, '.',   '.',     '-'
@@ -13,22 +13,37 @@ bed_df <- tibble::tribble(
 
 test_that("left arg works", {
   dist <- 100
-  out <- bed_df %>% bed_slop(genome, left = dist)
-  expect_true(all(bed_df$start - out$start == dist))
+  out <- x %>% bed_slop(genome, left = dist)
+  expect_true(all(x$start - out$start == dist))
 })
 
 test_that("right arg works", {
   dist <- 100
-  out <- bed_df %>% bed_slop(genome, right = dist)
-  expect_true(all(out$end - bed_df$end == dist))
+  out <- x %>% bed_slop(genome, right = dist)
+  expect_true(all(out$end - x$end == dist))
 })
 
 test_that("both arg works", {
   dist <- 100
-  out <- bed_df %>% bed_slop(genome, both = dist)
-  expect_true(all(bed_df$start - out$start == dist))
-  expect_true(all(out$end - bed_df$end == dist))
+  out <- x %>% bed_slop(genome, both = dist)
+  expect_true(all(x$start - out$start == dist))
+  expect_true(all(out$end - x$end == dist))
 })
 
-# test fraction
-# test off chrom
+test_that("both with fraction works", {
+  res <- bed_slop(x, genome, both = 0.5, fraction = TRUE)
+  expect_equal(res$start, c(250, 750))
+  expect_equal(res$end, c(1250, 1750))
+})
+
+test_that("left, fraction, strand works", {
+  res <- bed_slop(x, genome, left = 0.5, fraction = TRUE, strand = TRUE)
+  expect_equal(res$start, c(250, 1000))
+  expect_equal(res$end, c(1000, 1750))
+})
+
+test_that("right, fraction, strand works", {
+  res <- bed_slop(x, genome, right = 0.5, fraction = TRUE, strand = TRUE)
+  expect_equal(res$start, c(500, 750))
+  expect_equal(res$end, c(1250, 1500))
+})
