4d91b34cb087e6f75d00c168c45fbc5b45e86948
Kent Riemondy <kent.riemondy@gmail.com>
2016-09-28 17:26:34 -0600

    launch shiny helper fxn

---
+++ R/launch_shiny.r
@@ -0,0 +1,10 @@
+#' @export
+launch_shiny <- function() {
+  #http://deanattali.com/2015/04/21/r-package-shiny-app/
+  appDir <- system.file("shiny", package = "valr")
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `mypackage`.", call. = FALSE)
+  }
+  
+  shiny::runApp(appDir, display.mode = "showcase")
+}
