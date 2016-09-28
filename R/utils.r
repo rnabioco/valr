63f5b0b7f147ecbd63daf80bcba225208047f53f
Jay Hesselberth <jay.hesselberth@gmail.com>
2016-09-28 09:33:24 -0600

    WIP refactoring plot fxns

--- R/utils.r
+++ R/utils.r
@@ -1,4 +1,5 @@
 #' provides working directory for valr example files
+#' 
 #' @export
 valr_example <- function(path) {
   # https://twitter.com/JennyBryan/status/780150538654527488
@@ -7,25 +8,22 @@ valr_example <- function(path) {
 
 
 #' convienence function for generating example bed glyphs
-#' @param x tbl of intervals passed to .fun
-#' @param y optional tbl of intervals to pass to .fun  (default = FALSE)
-#' @param .fun valr function to execute on (x and/or y intervals)
-#' @param ... other arguments to pass to .fun (x ivls and y ivls passed by default if supplied) 
-#' @param x_name name of plotted x intervals (default is "X")
-#' @param y_name name of plotted y intervals (default is "Y")
-#' @param start_col name of column containing start positions to use for plotting results (default = "start")
-#' @param end_col name of columm containing end positions to use for plotting results (default = "end")
-#' @param title_ops options to pass to title display in ggplot object (default = "(x, y"))
+#'
+#' @param expr valr expression 
+#' 
 #' @return \code{ggplot2_obj}
+#'
+#' @examples 
+#' 
+#' bed_glyph(bed_intersect(x, y))
+#' 
 #' @export
-bed_glyph <- function(x, y = NULL, 
-                      .fun, ...,
-                      x_name = "X",
-                      y_name = "Y",
+bed_glyph <- function(expr,
                       start_col = "start",
-                      end_col = "end",
-                      title_ops = "(x, y)") {
+                      end_col = "end") {
 
+  args <- deparse(substitute(expr))
+ 
   if (!is.null(y)){
     x <- bed_cluster(x)
     x <- group_by(x, .id) 
@@ -39,7 +37,7 @@ bed_glyph <- function(x, y = NULL,
     y <- ungroup(y)
     res <- eval(.fun(x, y, ...)) 
     res <- mutate(res, bin = 1, 
-                  title = "Result")
+                  label = "Result")
     if ("start.x" %in% colnames(res)){
       res <- mutate_(res,
                      start = start_col,
@@ -74,31 +72,48 @@ bed_glyph <- function(x, y = NULL,
                                         levels = c(x_name,
                                                    "Result")))
   }
-  p_title = paste0(substitute(.fun), title_ops)
-  plot_glyph(comb, p_title)
+  title = paste0(substitute(expr))
+  glyph_plot(comb, title) + glyph_theme() + glyph_label()
 }
 
-#### plotting utility function (not exported)
-plot_glyph <- function(p_data, p_title){
-  ggplot2::ggplot(p_data) + 
+#' ggplot2 theme for glyphs
+#' 
+glyph_theme <- ggplot2::theme(
+  theme(
+    axis.line.y = element_blank(), 
+    axis.ticks.y = element_blank(),
+    axis.text.y = element_blank(),
+    legend.position = "none",
+    panel.grid = element_blank(),
+    panel.background = element_blank(),
+    plot.background = element_blank(),
+    panel.border = element_blank()
+  ) 
+)
+
+#' plotting utility function
+#' 
+glyph_plot <- function(data, title_plot){
+   
+  colors_default <- c('#fc8d59', '#ffffbf', '#91bfdb')
+  
+  ggplot2::ggplot(data) + 
     ggplot2::geom_rect(aes(xmin = start, xmax = end,
-                  ymin = bin, ymax = bin + 0.9,
-                  fill = title), color = "black", alpha = 0.75) + 
-    ggtitle(p_title) +
-    facet_grid(title ~ ., switch = "y", scales = "free_y", space = "free_y") +
-    scale_fill_manual(values = c("#fc8d59", 
-                                 "#ffffbf", 
-                                 "#91bfdb")) +
-    theme_bw() +
-    theme(
-      axis.line.y = element_blank(), 
-      axis.ticks.y = element_blank(),
-      axis.text.y = element_blank(),
-      legend.position = "none",
-      panel.grid = element_blank(),
-      panel.background = element_blank(),
-      plot.background = element_blank(),
-      panel.border = element_blank()
-    )
+                           ymin = bin, ymax = bin + 0.9,
+                           fill = title),
+                       color = "black", alpha = 0.75) + 
+    ggtitle(title_plot) +
+    facet_grid(label ~ ., switch = "y",
+               scales = "free_y", space = "free_y") +
+    scale_fill_manual(values = colors_default) +
+    theme_bw() + xlab('') + ylab('')
+
 }
 
+#' labels for glyph plot
+#' 
+glyph_label <- function(data) {
+  ggplot2::geom_label(data, aes((end - start) / 2 + start,
+                                bin + 0.5, label = .id)) + 
+    xlab("") + ylab("")
+}
