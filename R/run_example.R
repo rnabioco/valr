af94a61540d11385290703428f02369486940a8f
Kent Riemondy <kent.riemondy@gmail.com>
2016-09-28 17:25:51 -0600

    change run_example fxn to launch_shiny

--- R/run_example.R
+++
@@ -1,9 +0,0 @@
-#' @export
-run_example <- function() {
-  appDir <- system.file("shiny", package = "valr")
-  if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `mypackage`.", call. = FALSE)
-  }
-  
-  shiny::runApp(appDir, display.mode = "showcase")
-}
