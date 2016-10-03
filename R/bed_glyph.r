fbf1c801a80505de2361a41b24c96f4a2166dce1
Jay Hesselberth <jay.hesselberth@gmail.com>
2016-10-03 05:07:16 -0600

    more work on labels

--- R/bed_glyph.r
+++ R/bed_glyph.r
@@ -14,14 +14,15 @@
 #' )
 #'
 #' y <- tibble::tribble(
-#'   ~chrom, ~start, ~end,
-#'   'chr1', 30,     75
+#'   ~chrom, ~start, ~end, ~value,
+#'   'chr1', 30,     75,  50
 #' )
 #' 
 #' z <- tibble::tribble(
-#'   ~chrom, ~start, ~end,
-#'   'chr1', 30,     75,
-#'   'chr1', 50,     90
+#'   ~chrom, ~start, ~end, ~label,
+#'   'chr1', 30,     75,   '',
+#'   'chr1', 50,     90,   '',
+#'   'chr1', 91,     120,  'book-ended'
 #' )
 #' 
 #' genome <- tibble::tribble(
@@ -29,12 +30,12 @@
 #'   "chr1", 200
 #' ) 
 #'
-#' bed_glyph2(bed_intersect(x, y))
-#' bed_glyph2(bed_merge(z))
-#' bed_glyph2(bed_cluster(x))
+#' bed_glyph(bed_intersect(x, y))
+#' bed_glyph(bed_merge(z))
+#' bed_glyph(bed_cluster(x))
 #' 
 #' @export
-bed_glyph <- function(expr, res_name = 'result', label = NULL) {
+bed_glyph <- function(expr, label = NULL, res_name = 'result') {
 
   excl_params <- c('genome')
   
@@ -61,7 +62,7 @@ bed_glyph <- function(expr, res_name = 'result', label = NULL) {
     res <- bind_rows(res, rows)
   } 
 
-  # assign `y` values based on clustering
+  # assign `.y` values based on clustering
   res <- bed_cluster(res)
   res <- group_by(res, .facet, .id)
   res <- mutate(res, .y = row_number(.id))
@@ -70,16 +71,13 @@ bed_glyph <- function(expr, res_name = 'result', label = NULL) {
   # make res_name col last
   fct_names <- c(params, res_name)
   res <- mutate(res, .facet = factor(.facet, levels = fct_names))
-  
+ 
+  # plot title 
   title <- deparse(substitute(expr))
-  glyph_plot(res, title, label) + theme_glyph()
-}
-
-#' ggplot2 object for glyph
-#' @noRd
-glyph_plot <- function(res, title = '', label = NULL, colors = c("#fc8d59", "#ffffbf", "#91bfdb")) {
-  
-  plt <- ggplot(res) + 
+ 
+  fill_colors <- c("#fc8d59", "#ffffbf", "#91bfdb")
+ 
+  glyph <- ggplot(res) +
     geom_rect(aes_string(xmin = 'start', xmax = 'end',
                          ymin = '.y', ymax = '.y + 0.9',
                          fill = '.facet'),
@@ -87,16 +85,19 @@ glyph_plot <- function(res, title = '', label = NULL, colors = c("#fc8d59", "#ff
     facet_grid(.facet ~ ., switch = "y",
                scales = "free_y", space = "free_y") +
     ggtitle(title) +
-    scale_fill_manual(values = colors)
+    scale_fill_manual(values = fill_colors) +
+    xlab(NULL) + ylab(NULL)
   
   if (!is.null(label)) {
-    plt <- plt + geom_label(aes_string(x = '(end - start) / 2 + start',
-                                       y = '.y + 0.5',
-                                       label = 'label')) +
-      xlab(NULL) + ylab(NULL)
+    label <- as.name(label)
+    print(res)
+    aes_label <- aes_(x = quote((end - start) / 2 + start),
+                      y = quote(.y + 0.5),
+                      label = substitute(label))
+    glyph <- glyph + geom_label(aes_label)
   }
-  
-  plt
+
+  glyph + theme_glyph()
 }
 
 #' theme for glyphs 
