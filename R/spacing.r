53c8db3bb9166c30c27e500b30ea38b72cb4de52
Jay Hesselberth <jay.hesselberth@gmail.com>
2016-11-01 06:07:20 -0600

    add `interval_spacing`

---
+++ R/spacing.r
@@ -0,0 +1,34 @@
+#' Calculate interval spacing.
+#' 
+#' Overlapping intervals are merged. Spacing for the first interval of each
+#' chromosome is undefined (\code{NA}).
+#' 
+#' @param x tbl of intervals
+#'   
+#' @return \code{data_frame} with \code{.spacing} column.
+#' 
+#' @examples
+#' x <- tibble::tribble(
+#' ~chrom, ~start, ~end,
+#' 'chr1', 1,      100,
+#' 'chr1', 150,    200,
+#' 'chr2', 200,    300
+#' )
+#' 
+#' interval_spacing(x)
+#'   
+#' @export
+interval_spacing <- function(x) {
+  
+  if (! is_merged(x) ) 
+    res <- bed_merge(x)
+  
+  groups_x <- groups(x)
+  
+  res <- group_by(res, chrom)
+  res <- mutate(res, .spacing = start - lag(end)) 
+  
+  res <- group_by_(res, .dots = groups_x)
+  
+  res
+}
---
+++ tests/testthat/test_spacing.r
@@ -0,0 +1,15 @@
+context('spacing')
+
+x <- tibble::tribble(
+~chrom, ~start, ~end,
+'chr1', 1,      100,
+'chr1', 150,    200,
+'chr2', 200,    300
+)
+
+test_that('start intervals are NA', {
+  res <- interval_spacing(x)
+  first <- res %>% group_by(chrom) %>% slice(1) %>% select(chrom:end)
+  nas <- filter(res, is.na(.spacing)) %>% select(chrom:end)
+  expect_true(all(first == nas))
+})
