38b1fa959a13ddff8a6050088174ec0c390f4c69
kriemo <kent.riemondy@gmail.com>
2016-11-20 14:41:12 -0700

    projection test implementation (bed_projection()) #148 (#149)
    * bed_projection() initial implementation
    
    * add examples and clarify documentation
    
    * handle missing x or y chroms
    
    * flatten intervals to prevent counting overlaps more than once per x interval
    
    * add tests for bed_projection
    
    * remove space
    
    * fix error in tss tbl due to typo
    
    * update variable names
    
    * add example of bed_projection usage
    
    * add bed_projection
    
    * put pbinom is stats namespace
    
    * update globals
    
    * change to new variable names
    
    * fix typo
    
    * fix typos
    
    * add bed_projection()
    
    * remove whitespace and fix indentation
    
    * add bed_projection

---
+++ R/bed_projection.r
@@ -0,0 +1,125 @@
+#' Projection Test for query interval overlap.
+#' 
+#' @param x tbl of intervals
+#' @param y tbl of intervals
+#' @param genome chrom sizes
+#' @param by_chrom compute test per chromosome  rather than across whole genome
+#' 
+#' @template groups
+#' 
+#' @family interval-stats
+#' @return \code{data_frame} with the following columns:
+#'   \itemize{ 
+#'     \item{\code{chrom}} {the name of chromosome tested if \code{by_chrom} is \code{TRUE},
+#'      otherwise set to \code{whole_genome}}
+#'     \item{\code{p.value}} {p-value from a binomial test, note that p-values > 0.5
+#'      will be reported as 1 - p-value and \code{lower_tail} will be set to \code{FALSE}}
+#'     \item{\code{obs_exp_ratio}} {ratio of observed to expected overlap frequency}
+#'     \item{\code{lower_tail}} {\code{TRUE} denotes that the observed number of overlaps
+#'      is in the lower tail of the distribution (less overlap than expected), \code{FALSE}
+#'     denotes that the observed probabililty are in the upper tail of the distribution
+#'      (more overlap than expected)}
+#'     }
+#'   
+#' @seealso
+#'   \url{http://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1002529}
+#'   
+#' @examples 
+#' genome <- tibble::tribble(
+#'  ~chrom, ~size,
+#'  "chr1", 1e4,
+#'  "chr2", 2e4,
+#'  "chr3", 4e4
+#' )
+#' 
+#' x <- tibble::tribble(
+#'~chrom, ~start, ~end,
+#'"chr1", 100,    200,
+#'"chr1", 250,    400,
+#'"chr1", 500,    600,
+#'"chr1", 1000,   2000,
+#'"chr2", 100,    200
+#')
+#'
+#'y <- tibble::tribble(
+#'  ~chrom, ~start, ~end,
+#'  "chr1", 150,    175,
+#'  "chr1", 525,    575,
+#'  "chr1", 1100,   1200,
+#'  "chr1", 1400,   1600,
+#'  "chr2", 200,    1500
+#') 
+#'
+#'bed_projection(x, y, genome)
+#'bed_projection(x, y, genome, by_chrom = TRUE)
+#' @export
+bed_projection <- function(x, y, genome, by_chrom = FALSE) {
+
+  #find midpoints
+  x <- mutate(x, 
+              .midpoint = round((end + start) / 2),
+              start = .midpoint,
+              end = .midpoint + 1)
+  x <- select(x, -.midpoint)
+  
+  # flatten y intervals
+  y <- bed_merge(y)
+  
+  # count overlaps per chromosome, 
+  obs_counts <- bed_intersect(x, y)
+
+  # count overlaps
+  obs_counts <- group_by(obs_counts, chrom)
+  obs_counts <- summarize(obs_counts, .obs_counts = n())
+          
+  #total x intervals tested
+  total_counts <- group_by(x, chrom)
+  total_counts <- summarize(total_counts, .total_trials = n())
+  obs_counts <- full_join(obs_counts, total_counts, by = "chrom")
+  obs_counts <- mutate(obs_counts, .obs_counts = if_else(is.na(.obs_counts), 
+                                                         as.integer(0), .obs_counts))
+  
+  # calculate probabilty of overlap by chance
+  y <- mutate(y, 
+              .length = end - start)
+  y <- group_by(y, chrom)
+  y <- summarize(y, .reference_coverage = sum(.length))
+  # add in any missing chromosomes
+  y <- full_join(y, genome, by = "chrom")
+  
+  null_dist <- mutate(y, .exp_prob = .reference_coverage / size)
+ 
+  res <- inner_join(obs_counts, null_dist, by = "chrom")
+  
+  # binomial test and obs/exp
+  if (by_chrom){
+    res <- group_by(res, chrom)
+    res <- summarize(res,
+                     p.value = stats::pbinom(q = .obs_counts, 
+                                      size = .total_trials,
+                                      prob = .exp_prob),
+                     obs_exp_ratio = (.obs_counts / .total_trials) / .exp_prob
+                     )
+  } else {
+    res <- ungroup(res)
+    res <- summarize(res,
+                     .obs_counts = sum(.obs_counts),
+                     .total_trials = sum(.total_trials),
+                     .exp_prob = sum(.reference_coverage) / sum(size))
+    
+    res <- summarize(res,
+                     chrom = "whole_genome",
+                     p.value = stats::pbinom(q = .obs_counts, 
+                                      size = .total_trials,
+                                      prob = .exp_prob),
+                     obs_exp_ratio = (.obs_counts / .total_trials) / .exp_prob)
+  }
+  res <- mutate(res,
+                lower_tail = if_else(p.value < .5,
+                                      "TRUE",
+                                      "FALSE"),
+                p.value = if_else(p.value < .5,
+                                  p.value,
+                                  1 - p.value))
+  res
+}  
---
+++ tests/testthat/test_projection.r
@@ -0,0 +1,118 @@
+context('bed_projection')
+
+genome <- tibble::tribble(
+  ~chrom, ~size,
+  "chr1", 1000,
+  "chr2", 1000
+ )
+ 
+x <- tibble::tribble(
+  ~chrom, ~start, ~end,
+  "chr1", 50,    75,
+  "chr1", 250,    400,
+  "chr1", 500,    600
+)
+
+y <- tibble::tribble(
+  ~chrom, ~start, ~end,
+  "chr1", 100,   200,
+  "chr1", 200,    800
+) 
+
+bed_projection(x, y, genome)
+bed_projection(x, y, genome, by_chrom = TRUE)
+
+test_that("basic projection test works", {
+  #compare output to pbinom( )function
+  # 2 of 3 hits, 7 of 10 chance
+  # 1- pbinom because pval > .5
+  
+  exp <- 1 - pbinom(2, 3, 7/10)
+  res <- bed_projection(x, y, genome)
+  expect_equal(res$p.value, exp)
+})
+
+test_that("projection per chromosome works (by_chrom = TRUE)", {
+  genome <- tibble::tribble(
+    ~chrom, ~size,
+    "chr1", 2000,
+    "chr2", 2000
+  ) 
+
+  x <- tibble::tribble(
+    ~chrom, ~start, ~end,
+    "chr1", 100,    200,
+    "chr1", 250,    400,
+    "chr1", 500,    600,
+    "chr1", 1000,   2000,
+    "chr2", 100,    200
+  )
+  
+  y <- tibble::tribble(
+    ~chrom, ~start, ~end,
+    "chr1", 150,    175,
+    "chr1", 525,    575,
+    "chr1", 1100,   1200,
+    "chr1", 1400,   1600,
+    "chr2", 100,    1500
+  ) 
+  res <- bed_projection(x, y, genome, by_chrom = TRUE)
+  expect_true(all(c("chr1", "chr2") %in% res$chrom))
+})
+
+test_that("report significant when intervals are underrepresented, .lower_tail = TRUE", {
+  sig <- pbinom(0, 4, .7)
+  
+  genome <- tibble::tribble(
+    ~chrom, ~size,
+    "chr1", 1000,
+    "chr2", 1000
+  ) 
+  
+  x <- tibble::tribble(
+    ~chrom, ~start, ~end,
+    "chr1", 10,    20,
+    "chr1", 25,    40,
+    "chr1", 50,    60,
+    "chr1", 100,   150
+  )
+  
+  y <- tibble::tribble(
+    ~chrom, ~start, ~end,
+    "chr1", 200,    275,
+    "chr1", 525,    575,
+    "chr1", 200,    900
+  ) 
+  res <- bed_projection(x, y, genome)
+  expect_true(res$lower_tail == TRUE)
+  expect_true(res$p.value < .5)
+  expect_true(res$p.value == sig)
+})
+
+test_that("report significant when intervals are overrepresented, .lower_tail = FALSE", {
+  sig <- 1 - pbinom(4, 4, .7)
+  genome <- tibble::tribble(
+    ~chrom, ~size,
+    "chr1", 1000,
+    "chr2", 1000
+  ) 
+  
+  x <- tibble::tribble(
+    ~chrom, ~start, ~end,
+    "chr1", 200,    225,
+    "chr1", 250,    400,
+    "chr1", 500,    600,
+    "chr1", 300,   350
+  )
+  
+  y <- tibble::tribble(
+    ~chrom, ~start, ~end,
+    "chr1", 200,    275,
+    "chr1", 525,    575,
+    "chr1", 200,    900
+  ) 
+  res <- bed_projection(x, y, genome)
+  expect_true(res$lower_tail == FALSE)
+  expect_true(res$p.value < .5)
+  expect_true(res$p.value == sig)
+})
