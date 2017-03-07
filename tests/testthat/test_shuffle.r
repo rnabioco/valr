6ba7da7ff1fa562f1817540e097d852e58728132
Austin Gillen <austingillen@gmail.com>
2017-03-06 21:03:03 -0700

    add seeds to random and shuffle tests for reproducibility

--- tests/testthat/test_shuffle.r
+++ tests/testthat/test_shuffle.r
@@ -7,15 +7,16 @@ genome <- tibble::tribble(
   "chr3", 1e8
 )
 
-x <- bed_random(genome, n = 100) %>% arrange(chrom, start) 
+x <- bed_random(genome, n = 100, seed = seed) %>% arrange(chrom, start)
+seed <- 1010486
 
 test_that('within = TRUE maintains chroms', {
-  res <- bed_shuffle(x, genome, within = TRUE)
+  res <- bed_shuffle(x, genome, within = TRUE, seed = seed)
   expect_true(all(x$chrom == res$chrom))
 })
 
 test_that('within = FALSE shuffles chroms', {
-  res <- bed_shuffle(x, genome, within = FALSE)
+  res <- bed_shuffle(x, genome, within = FALSE, seed = seed)
   expect_false(all(x$chrom == res$chrom))
 })
 
@@ -24,7 +25,7 @@ test_that('`incl` includes intervals',{
     ~chrom, ~start, ~end,
     "chr1", 10000,  1000000
   )
-  res <- bed_shuffle(x, genome, incl = incl)
+  res <- bed_shuffle(x, genome, incl = incl, seed = seed)
   expect_true(all(res$chrom == 'chr1'))
   expect_true(all(res$start >= 1e4))
   expect_true(all(res$end <= 1e6))
@@ -37,7 +38,7 @@ test_that('`excl` excludes intervals',{
     "chr2", 1,      10000000,
     "chr3", 1,      100000000
   )
-  res <- bed_shuffle(x, genome, excl = excl)
+  res <- bed_shuffle(x, genome, excl = excl, seed = seed)
   expect_true(all(res$chrom == 'chr1'))
   expect_false(any(res$chrom == 'chr2'))
   expect_false(any(res$chrom == 'chr3'))
@@ -45,14 +46,14 @@ test_that('`excl` excludes intervals',{
 })
 
 test_that('completely excluded intervals throw an error',{
-  # all intervals completely excluded 
+  # all intervals completely excluded
   excl <- tibble::tribble(
     ~chrom, ~start, ~end,
     "chr1", 1,      1000000,
     "chr2", 1,      10000000,
     "chr3", 1,      100000000
   )
-  expect_error(bed_shuffle(x, genome, excl = excl))
+  expect_error(bed_shuffle(x, genome, excl = excl, seed = seed))
 })
 
 test_that('`incl` and `excl` are handled', {
@@ -65,7 +66,7 @@ test_that('`incl` and `excl` are handled', {
     ~chrom, ~start, ~end,
     "chr1", 1,  1000000
   )
-  res <- bed_shuffle(x, genome, incl, excl)
+  res <- bed_shuffle(x, genome, incl, excl, seed = seed)
   expect_true(all(res$chrom == 'chr1'))
   expect_true(all(res$start > 500000))
 })
@@ -79,7 +80,7 @@ test_that('empty intervals derived from `incl` and `excl` is handled', {
     ~chrom, ~start, ~end,
     "chr1", 1,  1000000
   )
-  expect_error(bed_shuffle(x, genome, incl, excl))
+  expect_error(bed_shuffle(x, genome, incl, excl, seed = seed))
 })
 
 test_that('exceeding `max_tries` yields an error', {
@@ -90,24 +91,23 @@ test_that('exceeding `max_tries` yields an error', {
     "chr2", 1,      1e7,
     "chr3", 1,      1e8
   )
-  expect_error(bed_shuffle(x, genome, excl = excl))
+  expect_error(bed_shuffle(x, genome, excl = excl, seed = seed))
 })
 
 test_that('`seed` generates reproducible intervals',{
-   seed <- 1010486
    res1 <- bed_shuffle(x, genome, seed = seed)
    res2 <- bed_shuffle(x, genome, seed = seed)
-   expect_identical(res1, res2) 
+   expect_identical(res1, res2)
 })
 
 test_that('all supplied x interval columns are passed to the result', {
-  
+
     x <- tibble::tribble(
       ~chrom,   ~start,    ~end, ~name, ~score, ~strand,
       "chr1", 80, 100,    "q1",   1,  "+"
     )
-    
-  res <- bed_shuffle(x, genome)
+
+  res <- bed_shuffle(x, genome, seed = seed)
   expect_true(all(c("strand", "score", "name", "start") %in% colnames(res)))
 })
 
