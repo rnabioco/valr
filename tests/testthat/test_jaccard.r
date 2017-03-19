a4384ccda641a54964bf9ab89279345e77df1fa9
Jay Hesselberth <jay.hesselberth@gmail.com>
2017-03-19 16:18:40 -0600

    Merge intervals and use numeric in bed_jaccard (#206)
    - closes #204

--- tests/testthat/test_jaccard.r
+++ tests/testthat/test_jaccard.r
@@ -15,3 +15,13 @@ test_that("jaccard coeff is calculated correctly", {
   res <- bed_jaccard(x, y)
   expect_equal(res$jaccard, 0.25)
 })
+
+test_that("jaccard coeff is calc'd for large data sets", {
+  genome <- read_genome(valr_example('hg19.chrom.sizes.gz'))
+
+  x <- bed_random(genome, n = 1e5, seed = 10000)
+  y <- bed_random(genome, n = 1e5, seed = 20000)
+
+  res <- bed_jaccard(x, y)
+  expect_equal(round(res$jaccard, 3), 0.016)
+})
