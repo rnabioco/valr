39abd49cbe4458024ff57a87cbe2107fd4779fcd
Jay Hesselberth <jay.hesselberth@gmail.com>
2017-05-24 05:40:33 -0600

    refactor makewindows - to get the speed up - some tests are wrong and need fixing

--- src/init.c
+++ src/init.c
@@ -14,7 +14,7 @@ extern SEXP valr_complement_impl(SEXP, SEXP);
 extern SEXP valr_coverage_impl(SEXP, SEXP);
 extern SEXP valr_intersect_impl(SEXP, SEXP, SEXP, SEXP);
 extern SEXP valr_merge_impl(SEXP, SEXP, SEXP);
-extern SEXP valr_makewindows_impl(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP valr_makewindows_impl(SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP valr_random_impl(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP valr_reldist_impl(SEXP, SEXP);
 extern SEXP valr_shuffle_impl(SEXP, SEXP, SEXP, SEXP, SEXP);
@@ -27,7 +27,7 @@ static const R_CallMethodDef CallEntries[] = {
   {"valr_coverage_impl",   (DL_FUNC) &valr_coverage_impl,   2},
   {"valr_intersect_impl",  (DL_FUNC) &valr_intersect_impl,  4},
   {"valr_merge_impl",      (DL_FUNC) &valr_merge_impl,      3},
-  {"valr_makewindows_impl",(DL_FUNC) &valr_makewindows_impl,6},
+  {"valr_makewindows_impl",(DL_FUNC) &valr_makewindows_impl,5},
   {"valr_random_impl",     (DL_FUNC) &valr_random_impl,     6},
   {"valr_reldist_impl",    (DL_FUNC) &valr_reldist_impl,    2},
   {"valr_shuffle_impl",    (DL_FUNC) &valr_shuffle_impl,    5},
