43e5a22b6a35c9c76135ebd7f6303a93ac723e1c
Jay Hesselberth <jay.hesselberth@gmail.com>
2017-07-16 06:01:14 -0600

    update rcpp

--- src/init.c
+++ src/init.c
@@ -8,32 +8,32 @@ Check these declarations against the C/Fortran source code.
 */
 
 /* .Call calls */
-extern SEXP valr_bed12toexons_impl(SEXP);
-extern SEXP valr_closest_impl(SEXP, SEXP, SEXP, SEXP);
-extern SEXP valr_complement_impl(SEXP, SEXP);
-extern SEXP valr_coverage_impl(SEXP, SEXP);
-extern SEXP valr_dist_impl(SEXP, SEXP, SEXP);
-extern SEXP valr_flank_impl(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP valr_intersect_impl(SEXP, SEXP, SEXP, SEXP);
-extern SEXP valr_makewindows_impl(SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP valr_merge_impl(SEXP, SEXP, SEXP);
-extern SEXP valr_random_impl(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP valr_shuffle_impl(SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP valr_subtract_impl(SEXP, SEXP);
+extern SEXP _valr_bed12toexons_impl(SEXP);
+extern SEXP _valr_closest_impl(SEXP, SEXP, SEXP, SEXP);
+extern SEXP _valr_complement_impl(SEXP, SEXP);
+extern SEXP _valr_coverage_impl(SEXP, SEXP);
+extern SEXP _valr_dist_impl(SEXP, SEXP, SEXP);
+extern SEXP _valr_flank_impl(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _valr_intersect_impl(SEXP, SEXP, SEXP, SEXP);
+extern SEXP _valr_makewindows_impl(SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _valr_merge_impl(SEXP, SEXP, SEXP);
+extern SEXP _valr_random_impl(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _valr_shuffle_impl(SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _valr_subtract_impl(SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
-  {"valr_bed12toexons_impl", (DL_FUNC) &valr_bed12toexons_impl, 1},
-  {"valr_closest_impl",      (DL_FUNC) &valr_closest_impl,      4},
-  {"valr_complement_impl",   (DL_FUNC) &valr_complement_impl,   2},
-  {"valr_coverage_impl",     (DL_FUNC) &valr_coverage_impl,     2},
-  {"valr_dist_impl",         (DL_FUNC) &valr_dist_impl,         3},
-  {"valr_flank_impl",        (DL_FUNC) &valr_flank_impl,        8},
-  {"valr_intersect_impl",    (DL_FUNC) &valr_intersect_impl,    4},
-  {"valr_makewindows_impl",  (DL_FUNC) &valr_makewindows_impl,  5},
-  {"valr_merge_impl",        (DL_FUNC) &valr_merge_impl,        3},
-  {"valr_random_impl",       (DL_FUNC) &valr_random_impl,       6},
-  {"valr_shuffle_impl",      (DL_FUNC) &valr_shuffle_impl,      5},
-  {"valr_subtract_impl",     (DL_FUNC) &valr_subtract_impl,     2},
+  {"_valr_bed12toexons_impl", (DL_FUNC) &_valr_bed12toexons_impl, 1},
+  {"_valr_closest_impl",      (DL_FUNC) &_valr_closest_impl,      4},
+  {"_valr_complement_impl",   (DL_FUNC) &_valr_complement_impl,   2},
+  {"_valr_coverage_impl",     (DL_FUNC) &_valr_coverage_impl,     2},
+  {"_valr_dist_impl",         (DL_FUNC) &_valr_dist_impl,         3},
+  {"_valr_flank_impl",        (DL_FUNC) &_valr_flank_impl,        8},
+  {"_valr_intersect_impl",    (DL_FUNC) &_valr_intersect_impl,    4},
+  {"_valr_makewindows_impl",  (DL_FUNC) &_valr_makewindows_impl,  5},
+  {"_valr_merge_impl",        (DL_FUNC) &_valr_merge_impl,        3},
+  {"_valr_random_impl",       (DL_FUNC) &_valr_random_impl,       6},
+  {"_valr_shuffle_impl",      (DL_FUNC) &_valr_shuffle_impl,      5},
+  {"_valr_subtract_impl",     (DL_FUNC) &_valr_subtract_impl,     2},
   {NULL, NULL, 0}
 };
 
