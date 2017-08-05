2eba6829be209b1d33923c67eb66c2353f6d466b
Jay Hesselberth <jay.hesselberth@gmail.com>
2017-08-05 08:42:55 -0700

    migrate to dplyr v0.7 headers (WIP) - copy complete set of headers from v0.7.2 - delay inclusion of GroupedDataFrame to fix missing build_index_cpp (modified dplyr_types.h; see unmerged dplyr/#2687) - updated to GroupedSlicingIndex and .names() form

--- inst/include/tools/Encoding.h
+++ inst/include/tools/Encoding.h
@@ -5,6 +5,41 @@
 #define BYTES_MASK (1<<1)
 #define LATIN1_MASK (1<<2)
 #define UTF8_MASK (1<<3)
+#define ASCII_MASK (1<<6)
+
+struct sxpinfo_struct {
+  // *INDENT-OFF*
+  SEXPTYPE type    :  TYPE_BITS;/* ==> (FUNSXP == 99) %% 2^5 == 3 == CLOSXP
+                                 * -> warning: `type' is narrower than values
+                                 *              of its type
+                                 * when SEXPTYPE was an enum */
+  // *INDENT-ON*
+  unsigned int obj   :  1;
+  unsigned int named :  2;
+  unsigned int gp    : 16;
+  unsigned int mark  :  1;
+  unsigned int debug :  1;
+  unsigned int trace :  1;  /* functions and memory tracing */
+  unsigned int spare :  1;  /* currently unused */
+  unsigned int gcgen :  1;  /* old generation number */
+  unsigned int gccls :  3;  /* node class */
+}; /*		  Tot: 32 */
+
+#ifndef IS_BYTES
+#define IS_BYTES(x) (reinterpret_cast<sxpinfo_struct*>(x)->gp & BYTES_MASK)
+#endif
+
+#ifndef IS_LATIN1
+#define IS_LATIN1(x) (reinterpret_cast<sxpinfo_struct*>(x)->gp & LATIN1_MASK)
+#endif
+
+#ifndef IS_ASCII
+#define IS_ASCII(x) (reinterpret_cast<sxpinfo_struct*>(x)->gp & ASCII_MASK)
+#endif
+
+#ifndef IS_UTF8
+#define IS_UTF8(x) (reinterpret_cast<sxpinfo_struct*>(x)->gp & UTF8_MASK)
+#endif
 
 // that bit seems unused by R. Just using it to mark
 // objects as Shrinkable Vectors
@@ -14,61 +49,17 @@
 // it if needed. See the maybe_copy method in DelayedProcessor
 #define DPLYR_SHRINKABLE_MASK (1<<8)
 
-struct sxpinfo_struct {
-    SEXPTYPE type      :  TYPE_BITS;/* ==> (FUNSXP == 99) %% 2^5 == 3 == CLOSXP
-			     * -> warning: `type' is narrower than values
-			     *              of its type
-			     * when SEXPTYPE was an enum */
-    unsigned int obj   :  1;
-    unsigned int named :  2;
-    unsigned int gp    : 16;
-    unsigned int mark  :  1;
-    unsigned int debug :  1;
-    unsigned int trace :  1;  /* functions and memory tracing */
-    unsigned int spare :  1;  /* currently unused */
-    unsigned int gcgen :  1;  /* old generation number */
-    unsigned int gccls :  3;  /* node class */
-}; /*		    Tot: 32 */
+#define IS_DPLYR_SHRINKABLE_VECTOR(x) (reinterpret_cast<sxpinfo_struct*>(x)->gp & DPLYR_SHRINKABLE_MASK)
+#define SET_DPLYR_SHRINKABLE_VECTOR(x) (reinterpret_cast<sxpinfo_struct*>(x)->gp |= DPLYR_SHRINKABLE_MASK)
+#define UNSET_DPLYR_SHRINKABLE_VECTOR(x) (reinterpret_cast<sxpinfo_struct*>(x)->gp &= (~DPLYR_SHRINKABLE_MASK) )
 
-#ifndef IS_BYTES
-# define IS_BYTES(x) (reinterpret_cast<sxpinfo_struct*>(x)->gp & BYTES_MASK)
-#endif
 
-#ifndef IS_LATIN1
-# define IS_LATIN1(x) (reinterpret_cast<sxpinfo_struct*>(x)->gp & LATIN1_MASK)
-#endif
+namespace dplyr {
 
-#ifndef IS_ASCII
-# define IS_ASCII(x) (reinterpret_cast<sxpinfo_struct*>(x)->gp & ASCII_MASK)
-#endif
-
-#ifndef IS_UTF8
-# define IS_UTF8(x) (reinterpret_cast<sxpinfo_struct*>(x)->gp & UTF8_MASK)
-#endif
-
-namespace dplyr{
-
-    enum encoding {
-       BYTES, LATIN1, UTF8, UNKNOWN
-    } ;
-
-    inline encoding get_encoding( SEXP s){
-        if( IS_BYTES(s) ) return BYTES ;
-        if( IS_LATIN1(s) ) return LATIN1 ;
-        if( IS_UTF8(s) ) return UTF8 ;
-        return UNKNOWN ;
-    }
-
-    inline const char* human_readable_encoding( encoding e ){
-        switch(e){
-        case BYTES: return "bytes" ;
-        case LATIN1: return "latin1" ;
-        case UTF8: return "UTF-8" ;
-        default: break ;
-        }
-        return "unknown" ;
-    }
+CharacterVector reencode_factor(IntegerVector x);
+CharacterVector reencode_char(SEXP x);
 
 }
+
 
 #endif
