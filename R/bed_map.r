1e3c4f10ffd07205ef37b67ccd321c4771c56c9c
Jay Hesselberth <jay.hesselberth@gmail.com>
2017-09-06 14:53:10 -0600

    use new intersect_impl to speed-up bed_map. - testing shows a speed-up of about 1 second (2680 ms before, 1680 ms current). - need to update tests

--- R/bed_map.r
+++ R/bed_map.r
@@ -1,15 +1,13 @@
-#' Calculate summaries and statistics from overlapping intervals.
+#' Calculate summaries from overlapping intervals.
 #'
-#' Used to apply functions like [min()], [count()] or [concat()] to intersecting
+#' Used to apply functions like [min()] and [count()] to intersecting
 #' intervals. Book-ended intervals are not reported by default, but can be
 #' included by setting `min_overlap = 0`.
 #'
 #' @param x [tbl_interval()]
 #' @param y  [tbl_interval()]
-#' @param invert report `x` intervals not in `y`
-#' @param suffix colname suffixes in output
-#' @param min_overlap minimum overlap for intervals.
 #' @param ... name-value pairs specifying colnames and expressions to apply
+#' @param min_overlap minimum overlap for intervals.
 #'
 #' @template groups
 #'
@@ -76,43 +74,35 @@
 #' bed_map(x, y, .vals.unique = values_unique(value))
 #'
 #' @export
-bed_map <- function(x, y, ..., invert = FALSE,
-                    suffix = c(".x", ".y"),
-                    min_overlap = 1) {
+bed_map <- function(x, y, ..., min_overlap = 1) {
   if (!is.tbl_interval(x)) x <- as.tbl_interval(x)
   if (!is.tbl_interval(y)) y <- as.tbl_interval(y)
 
-  groups_x <- groups(x)
+  suffix <- list(x = ".x", y = ".y")
+  x_nms <- str_c(names(x), suffix$x)
+  x_nms <- x_nms[!x_nms %in% "chrom.x"]
 
-  # used only to get the `x` suffix; `y` suffix is ignored`
-  suffix <- list(x = suffix[1], y = suffix[2])
+  x <- group_by(x, chrom, add = TRUE)
+  y <- group_by(y, chrom, add = TRUE)
 
-  # `x` names are suffixed to use for grouping later
-  x_names <- colnames(x)[!colnames(x) %in% "chrom"]
-  x_names_suffix <- stringr::str_c(x_names, suffix$x)
+  res <- intersect_impl(x, y, invert = TRUE, suffix_x = ".x", suffix_y = "")
 
-  # note that `y` columns have no suffix so can be referred to by the original
-  # names
-  res <- bed_intersect(x, y, invert = invert, suffix = c(suffix$x, ""))
+  res_noint <- filter(res, is.na(.overlap))
+  res_noint <- select(res_noint, chrom, ends_with('.x'))
 
-  res <- filter(res, .overlap >= min_overlap)
+  ## map supplied functions to each set of intervals
+  res_int <- filter(res, !is.na(.overlap))
+  res_int <- group_by(res_int, !!! syms(c("chrom", x_nms)))
+  res_int <- summarize(res_int, !!! quos(...))
+  res_int <- ungroup(res_int)
 
-  ##  map supplied functions to each set of intervals
-  res <- group_by(res, !!! rlang::syms(c("chrom", x_names_suffix)))
-  res <- summarize(res, !!! rlang::quos(...))
-  res <- ungroup(res)
-  ## remove x suffix, but don't pattern match with '.' regex
-  names_no_x <- stringr::str_replace(names(res), stringr::fixed(suffix$x), "")
-  names(res) <- names_no_x
-
-  # find rows of `x` that did not intersect
-  x_not <- anti_join(x, res, by = c("chrom", x_names))
-
-  res <- bind_rows(res, x_not)
+  res <- bind_rows(res_int, res_noint)
+  res <- select(
+    res, chrom,
+    start = start.x,
+    end = end.x, everything()
+  )
   res <- bed_sort(res)
-
-  # reassign original `x` groups. `y` groups are gone at this point
-  res <- group_by(res, !!! rlang::syms(c(groups_x)))
 
   res
 }
