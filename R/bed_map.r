2586328e2f9047936c8e423a480f17bbbef0d8b2
Jay Hesselberth <jay.hesselberth@gmail.com>
2018-01-24 20:12:28 -0700

    use new intersect_impl to speed-up bed_map. (#304)
    * use new intersect_impl to speed-up bed_map.
    - testing shows a speed-up of about 1 second (2680 ms before, 1680 ms current).
    - need to update tests
    
    * ignore profiling results
    
    * move examples to separate file
    
    * cleanup bed_map
    - 1 failing test. corner case where an x interval intersects 1 interval and book-ends another. The x interval is duplicated in the result.
    
    * comment
    
    * fix behavior for handling bookended intervals by tracking an interval integer id. Update tests to check for non-suffixed output colnames.
    
    * use contains() to drop passed column variable

--- R/bed_map.r
+++ R/bed_map.r
@@ -1,15 +1,18 @@
-#' Calculate summaries and statistics from overlapping intervals.
+#' Calculate summaries from overlapping intervals.
 #'
-#' Used to apply functions like [min()], [count()] or [concat()] to intersecting
-#' intervals. Book-ended intervals are not reported by default, but can be
-#' included by setting `min_overlap = 0`.
+#' Apply functions like [min()] and [count()] to intersecting intervals.
+#' [bed_map()] uses [bed_intersect()] to identify intersecting intervals, so
+#' output columns will be suffixed with `.x` and `.y`. Expressions that refer to
+#' input columns from `x` and `y` columns must take these suffixes into account.
+#'
+#' Book-ended intervals can be included by setting `min_overlap = 0`.
+#' Non-intersecting intervals from `x` are included in the result with `NA`
+#' values
 #'
 #' @param x [tbl_interval()]
 #' @param y  [tbl_interval()]
-#' @param invert report `x` intervals not in `y`
-#' @param suffix colname suffixes in output
+#' @param ... name-value pairs specifying column names and expressions to apply
 #' @param min_overlap minimum overlap for intervals.
-#' @param ... name-value pairs specifying colnames and expressions to apply
 #'
 #' @template groups
 #'
@@ -20,108 +23,66 @@
 #' @seealso
 #' \url{http://bedtools.readthedocs.io/en/latest/content/tools/map.html}
 #'
-#' @examples
-#' x <- trbl_interval(
-#'   ~chrom, ~start, ~end,
-#'   'chr1',      1,      100
-#' )
-#'
-#' y <- trbl_interval(
-#'   ~chrom, ~start, ~end, ~value,
-#'   'chr1', 1,      20,   10,
-#'   'chr1', 30,     50,   20,
-#'   'chr1', 90,     120,  30
-#' )
-#'
-#' bed_glyph(bed_map(x, y, value = sum(value)), label = 'value')
-#'
-#' x <- trbl_interval(
-#'  ~chrom, ~start, ~end,
-#'  'chr1', 100,    250,
-#'  'chr2', 250,    500
-#' )
-#'
-#' y <- trbl_interval(
-#'  ~chrom, ~start, ~end, ~value,
-#'  'chr1', 100,    250,  10,
-#'  'chr1', 150,    250,  20,
-#'  'chr2', 250,    500,  500
-#' )
-#'
-#' # also mean, median, sd etc
-#' bed_map(x, y, .sum = sum(value))
-#'
-#' bed_map(x, y, .min = min(value), .max = max(value))
-#'
-#' bed_map(x, y, .concat = concat(value))
-#'
-#' # create a list-col
-#' bed_map(x, y, .values = list(value))
-#'
-#' # can also use `nth` family from dplyr
-#' bed_map(x, y, .first = dplyr::first(value))
-#'
-#' bed_map(x, y, .last = dplyr::last(value))
-#'
-#' bed_map(x, y, .absmax = abs(max(value)))
-#'
-#' bed_map(x, y, .absmin = abs(min(value)))
-#'
-#' bed_map(x, y, .count = length(value))
-#'
-#' bed_map(x, y, .count_distinct = length(unique(value)))
-#'
-#' bed_map(x, y, .vals = values(value))
-#'
-#' bed_map(x, y, .vals.unique = values_unique(value))
-#'
-#' # counting defaults to output of NA instead of 0, different from Bedtools2 behavior
-#' bed_map(x, y, .counts = n())
-#'
-#' dplyr::mutate(bed_map(x, y, .counts = n()), .counts = ifelse(is.na(.counts), 0, .counts))
-#'
+#' @example inst/example/bed_map.r
 #'
 #' @export
-bed_map <- function(x, y, ..., invert = FALSE,
-                    suffix = c(".x", ".y"),
-                    min_overlap = 1) {
+bed_map <- function(x, y, ..., min_overlap = 1) {
   if (!is.tbl_interval(x)) x <- as.tbl_interval(x)
   if (!is.tbl_interval(y)) y <- as.tbl_interval(y)
 
-  groups_x <- groups(x)
+  ## add suffixes to all x columns except `chrom`
+  x_nms <- str_c(names(x)[!names(x) %in% "chrom"], ".x")
 
-  # used only to get the `x` suffix; `y` suffix is ignored`
-  suffix <- list(x = suffix[1], y = suffix[2])
+  ## add integer .id to track each input x ivl
+  ## don't use mutate, in order to ignore input grouping
+  ## don't overwrite .id if it exists
+  .id_col <- ".id"
+  if (.id_col %in% names(x)) { .id_col <- str_c(.id_col, ".x")}
+  x[[.id_col]] <- seq_len(nrow(x))
 
-  # `x` names are suffixed to use for grouping later
-  x_names <- colnames(x)[!colnames(x) %in% "chrom"]
-  x_names_suffix <- stringr::str_c(x_names, suffix$x)
+  .id_col_out <- str_c(.id_col, ".x")
 
-  # note that `y` columns have no suffix so can be referred to by the original
-  # names
-  res <- bed_intersect(x, y, invert = invert, suffix = c(suffix$x, ""))
+  ## add chrom as a group
+  x <- group_by(x, chrom, add = TRUE)
+  y <- group_by(y, chrom, add = TRUE)
 
-  res <- filter(res, .overlap >= min_overlap)
+  res <- intersect_impl(x, y, invert = TRUE, suffix_x = ".x", suffix_y= "")
 
-  ##  map supplied functions to each set of intervals
-  res <- group_by(res, !!! rlang::syms(c("chrom", x_names_suffix)))
-  res <- summarize(res, !!! rlang::quos(...))
-  res <- ungroup(res)
-  ## remove x suffix, but don't pattern match with '.' regex
-  names_no_x <- stringr::str_replace(names(res), stringr::fixed(suffix$x), "")
-  names(res) <- names_no_x
+  ## filter for rows that don't intersect. The `duplicated` call is required
+  ## because book-ended intervals in the intersect_impl result can
+  ## book-end multiple `y` intervals, causing them to be duplicated after the
+  ## `select`. base::duplicated is ~10x faster than dplyr::distinct
+  res_noint <- filter(res, is.na(.overlap) | .overlap < min_overlap)
+  res_noint <- select(res_noint, chrom, ends_with('.x'))
+  res_noint <- res_noint[!duplicated(res_noint[[.id_col_out]]), ]
 
-  # find rows of `x` that did not intersect
-  x_not <- anti_join(x, res, by = c("chrom", x_names))
+  ## identify intersecting intervals
+  res_int <- filter(res, !is.na(.overlap) & .overlap >= min_overlap)
 
-  res <- bind_rows(res, x_not)
+  ## drop non-intersecting intervals that are also in the intersecting set
+  ## this prevents duplicate reporting of an x interval if it both bookends
+  ## and overlaps y intervals. Using base R logical indexing here is ~ 7x faster
+  ## than dplyr::anti_join()
+  res_noint <- res_noint[!res_noint[[.id_col_out]] %in% res_int[[.id_col_out]], ]
+  res_noint <- select(res_noint, -contains(.id_col_out))
+
+  ## map supplied functions to each set of intersecting intervals
+  ## group_by .id_col_out to ensure that duplicated input x intervals are reported
+  res_int <- group_by(res_int, !!! syms(c("chrom", x_nms, .id_col_out)))
+  res_int <- summarize(res_int, !!! quos(...))
+  res_int <- ungroup(res_int)
+  res_int <- select(res_int, -contains(.id_col_out))
+
+  res <- bind_rows(res_int, res_noint)
+
+  ## rename to match input columns
+  colnames(res) <- stringr::str_replace(colnames(res), "[.]x$", "")
+
   res <- bed_sort(res)
-
-  # reassign original `x` groups. `y` groups are gone at this point
-  res <- group_by(res, !!! rlang::syms(c(groups_x)))
 
   res
 }
+
 
 #' @export
 #'
---
+++ inst/example/bed_map.r
@@ -0,0 +1,43 @@
+x <- trbl_interval(
+ ~chrom, ~start, ~end,
+ 'chr1', 100,    250,
+ 'chr2', 250,    500
+)
+
+y <- trbl_interval(
+ ~chrom, ~start, ~end, ~value,
+ 'chr1', 100,    250,  10,
+ 'chr1', 150,    250,  20,
+ 'chr2', 250,    500,  500
+)
+
+bed_glyph(bed_map(x, y, value = sum(value)), label = 'value')
+
+# summary examples
+bed_map(x, y, .sum = sum(value))
+
+bed_map(x, y, .min = min(value), .max = max(value))
+
+# identify non-intersecting intervals to include in the result
+res <- bed_map(x, y, .sum = sum(value))
+x_not <- bed_intersect(x, y, invert = TRUE)
+dplyr::bind_rows(res, x_not)
+
+# create a list-column
+bed_map(x, y, .values = list(value))
+
+# use `nth` family from dplyr
+bed_map(x, y, .first = dplyr::first(value))
+
+bed_map(x, y, .absmax = abs(max(value)))
+
+bed_map(x, y, .count = length(value))
+
+bed_map(x, y, .vals = values(value))
+
+# count defaults are NA not 0; differs from bedtools2 ...
+bed_map(x, y, .counts = n())
+
+# ... but NA counts can be coverted to 0's
+dplyr::mutate(bed_map(x, y, .counts = n()), .counts = ifelse(is.na(.counts), 0, .counts))
+
