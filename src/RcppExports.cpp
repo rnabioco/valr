8c46dc7190871ecc82aa9f4d82f5f29f7d5b0a56
Kent Riemondy <kent.riemondy@gmail.com>
2018-12-04 16:34:38 -0700

    move dplyr cpp to subdirectory, and require GNU make

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -210,9 +210,7 @@ static const R_CallMethodDef CallEntries[] = {
     {NULL, NULL, 0}
 };
 
-void init_hybrid_inline_map(DllInfo* /*dll*/);
 RcppExport void R_init_valr(DllInfo *dll) {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
-    init_hybrid_inline_map(dll);
 }
