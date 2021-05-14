6fd2591bb020868bae2caaadea2470432c1a9650
Jim Hester <james.f.hester@gmail.com>
2021-05-14 15:35:49 -0400

    Updates for readr 2.0.0
    in readr 2.0.0 newlines at the end of a file will generate a row of NA
    data, rather than being silently ignored. So we needed to modify the
    test data to avoid a test failure due to this.
    
    Additionally because readr 2.0.0 uses lazy parsing by default the
    warning would not be triggered during the initial read, but instead
    later when the malformed value is read. Setting `lazy = FALSE` instead
    reads the file eagerly and preserves the expected behavior.

--- tests/testthat/test_merge.r
+++ tests/testthat/test_merge.r
@@ -163,7 +163,9 @@ test_that("Test that precision default is high enough for formatting not to give
 })
 
 test_that("Test stranded merge with bedPlus files that have strand", {
-  expect_warning(x <- read_bed(valr_example("bug254_e.bed"), n_fields = 7, skip = 1))
+  skip_if(packageVersion("readr") <= "1.4.0")
+
+  expect_warning(x <- read_bed(valr_example("bug254_e.bed"), n_fields = 7, skip = 1, lazy = FALSE))
   x <- x %>% group_by(strand)
   res <- bed_merge(x, 200) %>% arrange(end)
   expect_equal(res$end, c(20000, 25000))
