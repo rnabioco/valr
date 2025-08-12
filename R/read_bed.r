4a62298ec4100d91a0ccf5675626ef812881c755
Jay Hesselberth <jay.hesselberth@gmail.com>
2025-08-12 07:46:09 -0600

    Correctly re-export cpp11bigwig methods

--- R/read_bed.r
+++ R/read_bed.r
@@ -182,43 +182,6 @@ bed12_coltypes <- list(
   exon_starts = readr::col_character()
 )
 
-
-#' Read a bigwig file into a valr compatible tbl
-#'
-#' This function will output a 4 column tibble with
-#' zero-based chrom, start, end, value columns.
-#'
-#' @param path path to bigWig file
-#' @param ... params for `cpp11bigwig::read_bigwig()`
-#'
-#' @examples
-#' read_bigwig(valr_example("hg19.dnase1.bw"))
-#'
-#' read_bigwig(valr_example("hg19.dnase1.bw"), as = "GRanges")
-#'
-#' @export
-read_bigwig <- function(path, ...) {
-  cpp11bigwig::read_bigwig(path, ...)
-}
-
-#' Read a bigBed file into a valr compatible tbl
-#'
-#' This function will output a tibble
-#' zero-based chrom, start, end, value columns,
-#' as well as any additional columns in the bigBed file.
-#'
-#' @param path path to bigBed file
-#' @param ... params for `cpp11bigwig::read_bigbed()`
-#'
-#' @examples
-#' read_bigbed(valr_example("test.bb"))
-#'
-#' @export
-read_bigbed <- function(path, ...) {
-  cpp11bigwig::read_bigbed(path, ...)
-}
-
-
 #' Import and convert a GTF/GFF file into a valr compatible bed tbl format
 #'
 #' @description
---
+++ R/reexport-cpp11bigwig.R
@@ -0,0 +1,7 @@
+#' @importFrom cpp11bigwig read_bigwig
+#' @export
+cpp11bigwig::read_bigwig
+
+#' @importFrom cpp11bigwig read_bigbed
+#' @export
+cpp11bigwig::read_bigbed
